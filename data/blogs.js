25a33e0f9dde905273231c7e76bfebe588e824eb
Ayushi Tyagi <70432336+ayushi-tyagi080@users.noreply.github.com>
2024-09-22 00:03:39 +0530

    Sample commit

---
+++ data/blogs.js
@@ -0,0 +1,45 @@
+blogs = [
+    {
+        title: "How to get started with JavaScript",
+        content: "This is content JavaScript",
+        slug: "javascript-learn"
+    },
+    {
+        title: "How to get started with React.js",
+        content: "This is content React.js",
+        slug: "reactjs-learn"
+    },
+    {
+        title: "How to get started with Node.js",
+        content: "This is content Node.js",
+        slug: "nodejs-learn"
+    },
+    {
+        title: "How to get started with Express.js",
+        content: "This is content Express.js",
+        slug: "expressjs-learn"
+    },
+    {
+        title: "How to get started with Python",
+        content: "This is content Python",
+        slug: "python-learn"
+    },
+    {
+        title: "How to get started with Js",
+        content: "This is content Js",
+        slug: "js-learn"
+    },
+    {
+        title: "How to get started with Django",
+        content: "This is content Django",
+        slug: "python-django-learn"
+    },
+    {
+        title: "How to get started with CSS",
+        content: "This is content Css",
+        slug: "css-learn"
+    },
+]
+
+
+module.exports = blogs;
